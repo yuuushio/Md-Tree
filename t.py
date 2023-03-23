767b9bd8aeba40ada2c13d550424aa9c90d1fd75
yush <>
2023-03-23 13:41:25 +1100

    write main (modularized) indent logic for the grid, works - not extensively tested

--- t.py
+++ t.py
@@ -8,6 +8,7 @@ class St:
         self.whitespace = 0
         self.depth = 0
         self.is_last = False
+        self.stripped_val = ""
 
     # for debugging purposes
     def print_val(self):
@@ -31,6 +32,16 @@ def read_file():
         data = f.readlines()
         return [St(d) for d in data]
 
+def assign_true_value(st_arr):
+
+    text = []
+
+    for i in st_arr:
+        # strips everything till the first character after the bullet point
+        tmp = re.sub(r"^\s*[-*+]\s+", "", i.val)
+        text.append(tmp.strip())
+    for i in range(len(text)):
+        st_arr[i].stripped_val = text[i]
 
 def assign_depth_and_whitespace(st_arr):
     whitespace_arr = []
@@ -102,13 +113,93 @@ def print_grid(d_arr):
     for li in d_arr:
         print(li)
 
+
+# print("│   " * (items[i].depth-1) + "└── " + text[i])
 def magic(arr):
-    # i'th item can have a max depth of i
+    # ^1: i'th item can have a max depth of i
     # item 0, d=0
     # item 1, d=0,1 
     # item 2, d=0,1,2
+
+    s_a = "├── "
+
+    s_b = "└── "
+    s_c = "│   "
+    s_d = "    "
+
+    # Initialize grid, depth_0 (j=0) will always have a pre-space/indent of ""
     grid = [[None if j!=0 else "" for j in range(len(arr))] for i in range(len(arr))]
+      
+    for i in range(1,len(arr)):
+        for j in range(len(arr)):
+            if j != 0 and arr[i].depth != 0:
+                if arr[i].depth == j and j == 1:
+                    pre_space = ""
+                    if not arr[i].is_last:
+                        pre_space = s_a
+                    elif grid[i-1][j] == s_a:
+                        pre_space = s_c
+                    elif arr[i].is_last and not arr[i].parent.is_last:
+                        pre_space = s_a
+                    elif arr[i].is_last and arr[i].parent.is_last:
+                        pre_space = s_b
+                    grid[i][j] = grid[i-1][j-1] + pre_space
+                elif j == 1:
+                    print(i,j,"found 2 1 ...")
+                    if grid[i-1][j] == s_c:
+                        grid[i][j] = s_c
+                    elif grid[i-1][j] == s_b:
+                        grid[i][j] = s_d
+                    if j < i:
+                        pre_space = ""
+                        if grid[i-1][j] == s_b:
+                            pre_space = s_d
+                        elif grid[i-1][j] == s_a:
+                            pre_space = s_c
+                        else:
+                            pre_space = s_d
+                        grid[i][j] = pre_space
+                elif arr[i].depth == j:
+                    pre_space = ""
+                    if arr[i].is_last:
+                        pre_space = s_b
+                    elif not arr[i].is_last:
+                        pre_space = s_a
+                    elif grid[i-1][j] == s_a:
+                        pre_space = s_c
+                    elif grid[i-1][j] == s_b:
+                        pre_space = s_d
+                    grid[i][j] = pre_space
+                elif j > arr[i].depth:
+                    # break inner loop as per rule ^1
+                    break
+                elif j < i:
+                    print(i,j)
+                    pre_space = ""
+                    if grid[i-1][j] == s_b:
+                        pre_space = s_d
+                    elif grid[i-1][j] == s_a:
+                        pre_space = s_c
+                    else:
+                        pre_space = s_d
+
+                    grid[i][j] = pre_space
+
+                else:
+                    # j == i
+                    # i-1][j-1] ...
+                    break
+
+
+
+
+
+
     print_grid(grid)
+    # for i,item in enumerate(arr):
+    #     print(grid[i][item.depth] + item.stripped_val + " ", item.is_last) 
+
+
 
 
 
@@ -137,6 +228,7 @@ def main():
     calc_if_last(st_arr)
     calc_parent(st_arr)
     # build_tree(st_arr)
+    assign_true_value(st_arr)
     magic(st_arr)
 
 
